eb77582b7503eddb82421a6802ad50f307b105b2
Emilia <emilia.myllykyla@hotmail.com>
2021-10-22 18:02:57 +0300

    Style Edit page to match other forms on the site. Add profile image upload.

--- src/components/Edit/EditModal.tsx
+++ src/components/Edit/EditModal.tsx
@@ -8,6 +8,9 @@ import { updateUser } from "../../features/profile/profileSlice";
 import { UserData } from "../../features/profile/types";
 import { history } from "../../history";
 import "./EditModal.css";
+import DropZone from "../Dropzone/DropZone";
+import placeholder from "../../images/placeholder_profile.png";
+import { uploadImage } from "../../features/image/imageSlice";
 
 type ChangeEvent =
   | React.ChangeEvent<HTMLInputElement>
@@ -27,31 +30,68 @@ const makeInitialData = (user: UserData | undefined) => ({
   imageUrl: user?.imageUrl || "",
 });
 
-const makedata = (uid: string, user: UserData, form: FormData) => ({
-  uid: uid,
-  following: user.following,
-  followers: user.followers,
-  ...form,
-});
+const makedata = (
+  uid: string,
+  user: UserData,
+  form: FormData,
+  imageUrl?: string
+) => {
+  const data = {
+    uid: uid,
+    following: user.following,
+    followers: user.followers,
+    ...form,
+  };
+  return imageUrl ? { ...data, imageUrl: imageUrl } : data;
+};
 
 const EditModal = ({ onClose }: { onClose: () => void }) => {
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState("");
   const dispatch = useAppDispatch();
   const authId = useAppSelector(selectAuthId);
   const user = useAppSelector(selectUserById(authId));
-  const [form, setForm] = useState<FormData>(makeInitialData(user));
+  const initialData = makeInitialData(user)
+  const [form, setForm] = useState<FormData>(initialData);
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     if (!authId || !user) return <Redirect to="/login" />;
     e.preventDefault();
-    const data = makedata(authId, user, form);
-    dispatch(update({ authId, username: data.username }));
-    dispatch(updateUser(data)).then(() => {
-      history.push(`/profile/${data.username}`);
+
+    const makeData = async () => {
+      // If no image file, make data with old profile image url
+      if (!imageFile) {
+        return makedata(authId, user, form);
+      } else {
+        // Else upload new image to Cloud Storage and make data with new url
+        return dispatch(uploadImage(imageFile))
+          .then((r) => {
+            // TODO: Delete old image from Coud Storage
+            return (r.payload as { url: string }).url
+          })
+          .then((url) => {
+            return makedata(authId, user, form, url);
+          });
+      }
+    };
+    // Then update userdata
+    makeData().then((data) => {
+      dispatch(update({ authId, username: data.username }));
+      dispatch(updateUser(data)).then(() => {
+        history.push(`/profile/${data.username}`);
+      });
     });
+
+    // DELETE OLD IMAGE FROM STORAGE
   };
 
   const handleChange = (e: ChangeEvent) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onFileDrop = (file: File) => {
+    setImageFile(file);
+    setImagePreview(URL.createObjectURL(file));
   };
 
   return (
@@ -61,7 +101,7 @@ const EditModal = ({ onClose }: { onClose: () => void }) => {
         <h1 className="edit-modal-h1">Edit Profile</h1>
         <img
           className="edit-profile-img"
-          src={form.imageUrl}
+          src={imagePreview || form.imageUrl || placeholder}
           alt="Edited profile"
         />
         <form className="edit-form">
@@ -86,15 +126,10 @@ const EditModal = ({ onClose }: { onClose: () => void }) => {
             />
           </div>
           <div>
-            <label htmlFor="imageUrl">New profile image URL: </label>
-            <input
-              id="imageUrl"
-              type="url"
-              name="imageUrl"
-              value={form.imageUrl}
-              onChange={handleChange}
-            />
+            <label>New profile image: </label>
+            <DropZone onFile={onFileDrop} file={imageFile} />
           </div>
+
           <button
             type="submit"
             onClick={handleSubmit}
